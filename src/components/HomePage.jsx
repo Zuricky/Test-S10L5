bb76efc6bf4b35d27eb2c07fecfd961c031521b2
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-14 15:32:05 +0100

    commit 8

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 function HomePage() {
   return (
     <div className="container my-4">
-      <h1>Welcome to UWF - Ultimate Weather Forecast</h1>
-      <Link to="/weather" className="btn btn-success">
+      <h1>Welcome to Ultimate Weather Forecast</h1>
+      <Link to="/weather" className="btn btn-primary">
         Check the weather forecast
       </Link>
     </div>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -18,7 +18,7 @@ function TopBar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link to="/Home" className="nav-link">
